3f5e112d886feca0f82df2fb8f373e8479d898e5
sanretu <maplekinoko@gmail.com>
2015-10-10 17:23:55 +0900

    autorun_bookmarklet.js
    bookmarklet

---
+++ autorun_bookmarklet.js
@@ -0,0 +1,9 @@
+(function(window,undefined){
+	var smp=document.getElementById('sample');
+	var btnNode=document.createElement('button');
+	btnNode.setAttribute('type','button');
+	btnNode.setAttribute('onclick','location.reload()');
+	var textNode=document.createTextNode('AutoRun');
+	btnNode.appendChild(textNode);
+	smp.appendChild(btnNode);
+})(window); 

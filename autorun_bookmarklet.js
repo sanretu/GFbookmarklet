799274b7c17857d3893abca41e1838bfc74433ea
sanretu <maplekinoko@gmail.com>
2015-10-10 18:37:02 +0900

    Update autorun_bookmarklet.js

--- autorun_bookmarklet.js
+++ autorun_bookmarklet.js
@@ -1,9 +1,34 @@
 (function(window,undefined){
-	var smp=document.getElementById('sample');
-	var btnNode=document.createElement('button');
-	btnNode.setAttribute('type','button');
-	btnNode.setAttribute('onclick','location.reload()');
-	var textNode=document.createTextNode('AutoRun');
-	btnNode.appendChild(textNode);
-	smp.appendChild(btnNode);
-})(window); 
+	var btnRun_timer;
+	var amb = document.getElementById('ambHeader');
+	var btnRunNode = document.createElement('button');
+	btnRunNode.setAttribute('type', 'button');
+	btnRunNode.setAttribute('onclick', 'autorun()');
+	btnRunNode.setAttribute('value', 'AutoRun');
+	var btnRunStopNode = document.createElement('button');
+	btnRunStopNode.setAttribute('type', 'button');
+	btnRunStopNode.setAttribute('onclick', 'autorun()');
+	btnRunStopNode.setAttribute('value', 'StopRun');
+	amb.appendChild(btnRunNode);
+	amb.appendChild(btnRunStopNode);
+
+	function autorun(){
+		var btnRun = document.getElementById('btnFight');
+		btnRun_timer = setInterval(autobtnclick(), 1000);
+	}
+
+	function autobtnclick(){
+		strstmn = document.getElementById('staminaCnt').innerHTML;
+		intstmn = strstmn.split("/");
+		if(intstmn[0] < 6){
+			var btnHerfbar = document.getElementById('staminaItem_24');
+			btnHerfbar.click();
+			var btnOk = document.getElementById('confirmPopOkBtn');
+			btnOk.click();
+		}
+	}
+
+	function stopautorun(){
+		clearInterval(btnRun_timer);
+	}
+})(window);
